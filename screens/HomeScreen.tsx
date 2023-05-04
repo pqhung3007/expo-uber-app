92488340af3be3cfdffbfa94f606b981b13796dc
Quang Hung <hungpham307@gmail.com>
2023-05-04 10:12:17 +0700

    feat: open a map on a selected location

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Image, View } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useDispatch } from "react-redux";
 
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import Options from "../components/Options";
+import { setDestination, setOrigin } from "../slices/navSlices";
 
 export default function HomeScreen() {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView className="bg-white h-full">
       <View className="p-4">
@@ -33,8 +37,19 @@ export default function HomeScreen() {
           placeholder="Where from?"
           query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
           nearbyPlacesAPI="GooglePlacesSearch"
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details?.geometry?.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
           onFail={(error) => console.error(error)}
           debounce={400}
+          minLength={2}
+          enablePoweredByContainer={false}
         />
 
         <Options />
