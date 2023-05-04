375926b79d2a7859aed9aa5dd20c26b6052c3aed
Quang Hung <hungpham307@gmail.com>
2023-05-04 11:03:22 +0700

    feat: display map on the map screen

---
+++ components/Map.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import MapView, { Marker } from "react-native-maps";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlices";
+
+export default function Map() {
+  const origin = useSelector(selectOrigin);
+  return (
+    <>
+      <MapView
+        className="flex-1"
+        initialRegion={{
+          latitude: origin?.location.lat || 21.027763,
+          longitude: origin?.location.lng || 105.83416,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.008,
+        }}
+      />
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+        />
+      )}
+    </>
+  );
+}
--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,10 +1,26 @@
+import { useNavigation } from "@react-navigation/native";
+import { Icon } from "@rneui/themed";
 import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, TouchableOpacity, View } from "react-native";
+import Map from "../components/Map";
 
 export default function MapScreen() {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View>
+        <TouchableOpacity
+          className="bg-slate-100 absolute top-16 left-4 z-20 rounded-full shadow-md p-3"
+          onPress={() => navigation.goBack()}
+        >
+          <Icon name="menu" />
+        </TouchableOpacity>
+
+        <View className="h-1/2">
+          <Map />
+        </View>
+        <View className="h-1/2"></View>
+      </View>
+    </SafeAreaView>
   );
 }
